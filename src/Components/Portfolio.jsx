795050acd114a7f30fa112b13f51fec5128402c4
Yasar Nazzarian <yasar.nazzarian.1@ens.etsmtl.ca>
2022-11-11 18:07:02 +0000

    some style changes🧑‍🎨

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -43,9 +43,8 @@ const projectList = [
   },
   {
     title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    description: "Created from Microsoft's resume workshop and deployed to GitHub pages.",
+    url: "https://yasar2019.github.io/MyResume/",
   },
 ];
 
