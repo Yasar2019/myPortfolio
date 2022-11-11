f47f11101b7917c28d3bd9eab733904a1d40eb5e
Yasar Nazzarian <yasar.nazzarian.1@ens.etsmtl.ca>
2022-11-11 01:38:41 +0000

    Updating <Portfolio>

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/joshua-aragon-BMnhuwFYr7w-unsplash.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,21 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "BluetoothMesh for controlling the UV light of an Horticulture Chamber  🎉",
+    description: "Software Developer Intern of Symphoni R&D team at Delight Canada Inc.",
+    url: "https://symphoni.ai/",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title:
+      "Designing and developping a new system for SPACQ (Professional Society of Authors and Composers of Quebec)",
+    description: "Realizing the Vision And SRS documents and offer a prototype to the client.",
+    url: "https://eportfolio.etsmtl.ca/Portfolio/Document/56904",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
 
