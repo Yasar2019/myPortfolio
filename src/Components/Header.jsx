43f555f7fdbdd03d7b2faf1ae5dbd19d4e09f0df
Yasar Nazzarian <yasar.nazzarian.1@ens.etsmtl.ca>
2026-06-03 16:10:10 -0400

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -103,10 +103,10 @@ const Ring = styled.span`
 `;
 
 const ProfileImage = styled.img`
-  width: 180px;
-  height: 180px;
-  border-radius: 50%;
+  width: 100%;
+  height: 100%;
   object-fit: cover;
+  display: block;
   box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
   margin-bottom: 1.5rem;
   position: relative;
